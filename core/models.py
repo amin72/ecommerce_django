37d424d633e8b403ab290b1f07a4f03e0a04dd39
Amin <asaa47721@gmail.com>
2020-01-18 04:40:09 +0330

    add new fields to Item model. makemigrations

--- core/models.py
+++ core/models.py
@@ -6,8 +6,24 @@ from django.conf import settings
 User = get_user_model()
 
 
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear'),
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger'),
+)
+
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
 
     def __str__(self):
         return self.title
@@ -16,7 +32,6 @@ class Item(models.Model):
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    price = models.FloatField()
 
     def __str__(self):
         return self.item.title
