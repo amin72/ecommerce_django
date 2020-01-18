4e7273db66d4832e07a33e89febbb21af2b02818
Amin <asaa47721@gmail.com>
2020-01-18 09:55:46 +0330

    add messages to add_to_cart and remove_from_cart views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
 from django.utils import timezone
+from django.contrib import messages
 from .models import Order, OrderItem, Item
 
 
@@ -29,14 +30,19 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
+            messages.info(request, "This item quantity was updated.")
+            return redirect('core:product', slug=slug)
         else:
+            messages.info(request, "This item was added to your cart.")
             order.items.add(order_item)
+            return redirect('core:product', slug=slug)
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(user=request.user,
             ordered_date=ordered_date)
         order.items.add(order_item)
-    return redirect('core:product', slug=slug)
+        messages.info(request, "This item was added to your cart.")
+        return redirect('core:product', slug=slug)
 
 
 
@@ -55,9 +61,11 @@ def remove_from_cart(request, slug):
                 ordered=False
             )[0]
             order.items.remove(order_item)
+            messages.info(request, "This item was removed from your cart.")
             return redirect('core:product', slug=slug)
         else:
+            messages.info(request, "This item was not in your cart.")
             return redirect('core:product', slug=slug)
     else:
-        # add a message saying the user doesn't have an order
+        messages.info(request, "You do not have an active order.")
         return redirect('core:product', slug=slug)
