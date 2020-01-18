1f96af9d04252eaf351f986ebca606bc4628397e
Amin <asaa47721@gmail.com>
2020-01-18 03:53:52 +0330

    define Item, OrderItem, Order models. makemigration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.item.title
+
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
