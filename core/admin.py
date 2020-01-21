484a3a08d04c1a6e4d999d91898b5a13272bb786
Amin <asaa47721@gmail.com>
2020-01-22 01:30:59 +0330

    add BillingAddress and Payment models to admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Order, OrderItem, Item
+from .models import Order, OrderItem, Item, Payment, BillingAddress
 
 
 admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
+admin.site.register(Payment)
+admin.site.register(BillingAddress)
