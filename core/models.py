52e4b428c2da020fada88cb9292a539bc3d5e24c
Amin <asaa47721@gmail.com>
2020-01-23 13:57:24 +0330

    check if there is a coupon or not for the order

--- core/models.py
+++ core/models.py
@@ -88,7 +88,11 @@ class Order(models.Model):
         total = 0
         for order_item in self.items.all():
             total += order_item.get_final_price()
-        total -= self.coupon.amount
+
+        # subtract coupon amount from total order price
+        if self.coupon:
+            total -= self.coupon.amount
+
         return total
 
 
