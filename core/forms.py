87e21ce3232200914159a1a70c6107897d2ef3f0
Amin <asaa47721@gmail.com>
2020-01-23 18:04:32 +0330

    define RefundForm

--- core/forms.py
+++ core/forms.py
@@ -42,4 +42,10 @@ class CouponForm(forms.Form):
         'aria-label': "Recipient's username",
         'aria-describedby': "basic-addon2"
     }))
-    
+
+
+
+class RefundForm(forms.Form):
+    ref_code = forms.CharField()
+    message = forms.Textarea()
+    email = forms.EmailField()
