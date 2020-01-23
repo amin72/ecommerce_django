34efc7e35e72c1935b1639cebee53c83bb72bda6
Amin <asaa47721@gmail.com>
2020-01-23 20:08:08 +0330

    define make_refund_accepted action

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Order, OrderItem, Item, Payment, BillingAddress, Coupon
 
 
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_requested=False, refund_granted=True)
+make_refund_accepted.short_description = 'Update orders to refund granted'
+
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
         'user',
@@ -34,6 +40,8 @@ class OrderAdmin(admin.ModelAdmin):
         'user__username',
         'ref_code',
     ]
+
+    actions = [make_refund_accepted]
 
 
 
