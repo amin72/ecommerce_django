4bea15be9bb07f56e7fac8d9ff59ecb3f7392c46
Amin <asaa47721@gmail.com>
2020-01-27 18:49:05 +0330

    set blank=True for some fields in Order model

--- core/models.py
+++ core/models.py
@@ -85,14 +85,14 @@ class Order(models.Model):
     
     shipping_address = models.ForeignKey('Address',
         related_name='shipping_address',
-        on_delete=models.SET_NULL, null=True)
+        on_delete=models.SET_NULL, blank=True, null=True)
     
     billing_address = models.ForeignKey('Address',
         related_name='billing_address',
-        on_delete=models.SET_NULL, null=True)
+        on_delete=models.SET_NULL, blank=True, null=True)
     
-    payment = models.ForeignKey('Payment', on_delete=models.SET_NULL, null=True)
-    coupon = models.ForeignKey('Coupon', on_delete=models.SET_NULL, null=True)
+    payment = models.ForeignKey('Payment', on_delete=models.SET_NULL, blank=True, null=True)
+    coupon = models.ForeignKey('Coupon', on_delete=models.SET_NULL, blank=True, null=True)
     being_delivered = models.BooleanField(default=False)
     received = models.BooleanField(default=False)
     refund_requested = models.BooleanField(default=False)
